a5d4d167fcbcc9d879f4075ee9f398cafb263bdd
Shuaicheng Zhu <jason23347@163.com>
2025-10-22 14:10:05 +0800

    Add clear_line() & move_up_head()

--- src/terminal.h
+++ src/terminal.h
@@ -5,18 +5,15 @@
 
 #ifdef WITH_COLOR
 
-#define NORMAL    0
-#define BG_NORMAL ;NORMAL
-
-#define BLACK     30
-#define RED       31
-#define GREEN     32
-#define YELLOW    33
-#define BLUE      34
-#define PURPLE    35
-#define DARKGREEN 36
-#define WHITE     37
-
+#define NORMAL       0
+#define BLACK        30
+#define RED          31
+#define GREEN        32
+#define YELLOW       33
+#define BLUE         34
+#define PURPLE       35
+#define DARKGREEN    36
+#define WHITE        37
 #define BG_BLACK     40
 #define BG_RED       41
 #define BG_GREEN     42
@@ -30,8 +27,11 @@
 
 #define color(code) "\033[" STR(code) "m"
 
-#define set_color(c)  (global_config.raw_output == 0) ? printf(color(%d), c) : ((void)0)
-#define reset_color() (global_config.raw_output == 0) ? printf(color(NORMAL;NORMAL)) : ((void)0)
+#define set_color(c)                                                           \
+    ((global_config.raw_output == 0) ? printf("\033[%dm", c) : ((void)0))
+#define reset_color()                                                          \
+    (global_config.raw_output == 0) ? printf("\033[%d;%dm", NORMAL, NORMAL)    \
+                                    : ((void)0)
 
 #else /* WITH_COLOR */
 
@@ -40,5 +40,10 @@
 #define reset_color(...)
 
 #endif /* WITH_COLOR */
+
+#define clear_line()                                                           \
+    (global_config.raw_output == 0) ? printf("\r\033[K") : ((void)0)
+#define move_up_head()                                                         \
+    (global_config.raw_output == 0) ? printf("\r\033[F") : ((void)0)
 
 #endif /* TERMINAL_H */
