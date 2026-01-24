102f6507741f9555e26c134f0f5fcc6fdc574427
Shuaicheng Zhu <jason23347@163.com>
2026-01-24 23:22:02 +0800

    opt(speedtest): Update SPEEDTEST_PORT to use HTTPS

--- src/conf.h
+++ src/conf.h
@@ -32,7 +32,7 @@ int print_log(enum LOG_LEVEL level, const char *fmt, ...);
 #define CIPPV6_PORT             80
 #define CIPPV6_PATH             "/get_ip.php"
 #define SPEEDTEST_DOMAIN        "speed.ustb.edu.cn"
-#define SPEEDTEST_PORT          80
+#define SPEEDTEST_PORT          443
 #define SPEEDTEST_UPLOAD_PATH   "/backend/empty.php"
 #define SPEEDTEST_DOWNLOAD_PATH "/backend/garbage.php"
 #define DRCOM_HOST              "zifuwu.ustb.edu.cn"
